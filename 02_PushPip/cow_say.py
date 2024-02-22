ede2bdb3c77cc9c4e9abf211f39ab1a827c0fab7
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-02-22 16:34:56 +0300

    Add flags for 'cow', 'eyes', 'tongue' and 'list'
    For basic message output it's necessary to use '-t' flag.
    This option was mentioned on this page - https://pypi.org/project/cowsay/ -
    in section 'Command Line Usage'.

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,7 +1,51 @@
+import sys
 import argparse
-import cowsay as cs
+from cowsay import cowsay, list_cows
 
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number")
-args = parser.parse_args()
-print(cs.cowsay(args.square))
+def main():
+    parser = argparse.ArgumentParser(description='Program similar to cowsay')
+    parser.add_argument(
+            '-t',
+            '--text',
+            help='The text to be said'
+            )
+    parser.add_argument(
+            '-f',
+            '--file',
+            help='Specify the cowfile',
+            default='default'
+            )
+    parser.add_argument(
+            '-e',
+            '--eyes',
+            help='Change the eyes', 
+            default='o0'
+            )
+    parser.add_argument(
+            '-T',
+            '--tongue',
+            help='The tongue file',
+            default='U'
+            )
+    parser.add_argument(
+            '-l',
+            '--list', 
+            action='store_true', 
+            help='List available cowfiles'
+            )
+    args = parser.parse_args()
+
+    if args.list:
+        print(list_cows())
+    else:
+        print(
+            cowsay(
+                args.text, 
+                cow=args.file,
+                eyes=args.eyes,
+                tongue=args.tongue
+                )
+            )
+
+if __name__ == '__main__':
+    main()
