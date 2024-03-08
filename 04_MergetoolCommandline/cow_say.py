074b74e8d210b5e5be9e1db774c3c22d398b755d
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-03-08 23:25:30 +0300

    Complete all possible functions
    For list_cow() method there is no completion. So only three completions
    are presented in this commit.
    By the way, in previous commit might '64c4' wrong make_bubble
    implementation.
    Also added custom intro :)

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,15 +1,15 @@
 from cowsay import cowsay, list_cows, make_bubble, cowthink, Bubble 
 import cmd
 import shlex
-
-def analyze(line):
-
-    def check_val(arg, arg_name, dic):
+    
+def check_val(arg, arg_name, dic, dtype=str):
         if arg[0] != '-':
-            dic[arg_name] = arg
+            dic[arg_name] = dtype(arg)
         else:
             raise ValueError(f'{arg_name} got wrong value: {arg}')
 
+
+def analyze(line):
     diction = dict()
     words = shlex.split(line)
     wit = iter(words)
@@ -32,8 +32,15 @@ def analyze(line):
                 diction['message'] = message 
     return diction
 
+HAILS = {'Hello', 'Mo-o-o', 'Hi'}
+COWS = {'banana', 'cat'}
+EYES = {'xx', 'Oo'}
+TONGUE = {'U', 'Y'}
+WIDTH = {'50', '75', '25'}
+
 class mycmd(cmd.Cmd):
-    prompt = '>>'
+    prompt = '>> '
+    intro = "\n\t\tDobro došli u presonalni CMD\n"
 
     def do_list_cows(self, arg):
         '''
@@ -73,6 +80,22 @@ class mycmd(cmd.Cmd):
         res = analyze(arg)
         print(cowthink(**res)) 
 
+    def complete_cowthink(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        DICT = []
+        match len(words):
+            case 2:
+                DICT = HAILS
+            case _:
+                if words[-2] == '-T' or words[-2] == '--tongue':
+                    DICT = TONGUE
+                elif words[-2] == '-f' or words[-2] == '--file':
+                    DICT = COWS
+                elif words[-2] == '-e' or words[-2] == '--eyes':
+                    DICT = EYES
+        res = [c for c in DICT if c.startswith(text) or text=='.']
+        return res
+
     def do_make_bubble(self, arg):
         ''' 
         make_bubble(
@@ -80,7 +103,7 @@ class mycmd(cmd.Cmd):
             text,
             brackets=Bubble(
                 stem='\\',
-                l='<',
+                l='<'
                 r='>',
                 tl='/',
                 tr='\\',
@@ -92,8 +115,8 @@ class mycmd(cmd.Cmd):
             width=40,
             wrap_text=True
             )
-        Wraps text is wrap_text is true, then pads text and sets inside a bubble.
-        This is the text that appears above the cows
+        Wraps text is wrap_text is true, then pads text and sets inside a
+        bubble. This is the text that appears above the cows
         ''' 
         words = shlex.split(arg)
         params = dict()
@@ -102,14 +125,28 @@ class mycmd(cmd.Cmd):
             match word:
                 case '--width':
                     width = next(wit)
-                    check_val(width, 'width', params)
+                    check_val(width, 'width', params, int)
                 case '--wrap_text':
                     wrap = next(wit)
-                    check_val(wrap, 'wrap_text', params)
+                    check_val(wrap, 'wrap_text', params, int)
                 case _:
                     message = word
-                    diction['message'] = message  
+                    params['text'] = message  
         print(make_bubble(**params))
+
+    def complete_make_bubble(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        DICT = []
+        match len(words):
+            case 2:
+                DICT = HAILS
+                res = [c for c in DICT if c.startswith(text) or text=='.']
+            case _ :
+                if words[-2] == '--width':
+                    DICT = WIDTH
+                    res = [c for c in DICT if str(c).startswith(text)
+                            or text=='.']
+        return res
 
     def do_cowsay(
             self,
@@ -124,8 +161,8 @@ class mycmd(cmd.Cmd):
             ) -> str:
         '''
         Similar to the cowsay command. Parameters are listed with their
-        corresponding options in the cowsay command. Returns the resulting cowsay
-        string
+        corresponding options in the cowsay command. Returns the resulting
+        cowsay string
 
         :param message: The message to be displayed
         :param cow: -f – the available cows can be found by calling list_cows
@@ -140,6 +177,22 @@ class mycmd(cmd.Cmd):
         '''
         res = analyze(message)
         print(cowsay(**res)) 
+
+    def complete_cowsay(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        DICT = []
+        match len(words):
+            case 2:
+                DICT = HAILS
+            case _:
+                if words[-2] == '-T' or words[-2] == '--tongue':
+                    DICT = TONGUE
+                elif words[-2] == '-f' or words[-2] == '--file':
+                    DICT = COWS
+                elif words[-2] == '-e' or words[-2] == '--eyes':
+                    DICT = EYES
+        res = [c for c in DICT if c.startswith(text) or text=='.']
+        return res
 
 if __name__ == '__main__':
     mycmd().cmdloop()
